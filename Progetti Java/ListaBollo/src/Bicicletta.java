b08561c45111b34e6bd6984ac471c5c8488042c9
Antonio Cesare Massimo Friuli <75506520+AlfaStrikers@users.noreply.github.com>
2023-02-16 12:00:05 +0100

    Aggiunta nuova cartella "Progetti Java"

---
+++ Progetti Java/ListaBollo/src/Bicicletta.java
@@ -0,0 +1,12 @@
+public class Bicicletta {
+
+    int bolloBici;
+
+
+    public Bicicletta(int bolloBici){
+        this.bolloBici = bolloBici;
+    }
+    public int getBolloBici() {
+        return bolloBici;
+    }
+}
