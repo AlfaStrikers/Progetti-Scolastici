9739cdc589230a60bb14a2b35ee01b95da8784b5
Antonio Cesare Massimo Friuli <75506520+AlfaStrikers@users.noreply.github.com>
2023-02-16 11:54:49 +0100

    Add files via upload

---
+++ Lista bollo 3 veicoli/src/Automobile.java
@@ -0,0 +1,11 @@
+public class Automobile {
+    int bolloAuto;
+
+    public Automobile(int bolloAuto){
+        this.bolloAuto = bolloAuto;
+    }
+    public int getBolloAuto() {
+        return bolloAuto;
+    }
+
+}
