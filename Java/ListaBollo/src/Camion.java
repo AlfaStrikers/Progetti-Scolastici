fdae45d92bbddd1196429dc06a3fbee183062bbd
Antonio Cesare Massimo Friuli <75506520+AlfaStrikers@users.noreply.github.com>
2023-02-16 17:21:06 +0100

    Aggiunta cartella ListaBollo con un esercizio

---
+++ Java/ListaBollo/src/Camion.java
@@ -0,0 +1,13 @@
+public class Camion {
+
+    int bolloCamion;
+
+    public Camion(int bolloCamion){
+        this.bolloCamion = bolloCamion;
+    }
+    public int getBolloCamion() {
+        return bolloCamion;
+    }
+
+
+}
