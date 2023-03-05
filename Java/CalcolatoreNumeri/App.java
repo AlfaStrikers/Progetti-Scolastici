cdb30f4a6a2841c1bb75114d5ccc8f91030dd58f
Antonio Cesare Massimo Friuli <75506520+AlfaStrikers@users.noreply.github.com>
2023-03-05 21:05:13 +0100

    Aggiunta cartella esercizio calcolatore

---
+++ Java/CalcolatoreNumeri/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        Calcolo calcolo = new Calcolo();
+        int somma = calcolo.sommaNumeri();
+        System.out.println("La somma dei due numeri inseriti e' : " + somma);
+    }
+}
---
+++ Java/CalcolatoreNumeri/Calcolo.java
@@ -0,0 +1,24 @@
+public class Calcolo {
+    int primoNumero = 50;
+    int secondoNumero = 50;
+
+    public int getPrimoNumero() {
+        return primoNumero;
+    }
+
+    public void setPrimoNumero(int primoNumero) {
+        this.primoNumero = primoNumero;
+    }
+
+    public int getSecondoNumero() {
+        return secondoNumero;
+    }
+
+    public void setSecondoNumero(int secondoNumero) {
+        this.secondoNumero = secondoNumero;
+    }
+
+    public int sommaNumeri() {
+        return primoNumero + secondoNumero;
+    }
+}
